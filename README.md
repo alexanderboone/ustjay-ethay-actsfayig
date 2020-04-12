d04a3d9fa15bd97a2f6f5f48bca08b38a936a55e
Alexander Boone <alboone@uw.edu>
2020-04-12 12:20:27 -0700

    Implemented fact-scraping and pig-latinizer functionality and added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pig Latinize a Random Fact
+
+Web app mashup built with flask that returns the URL to a pig-latinized random fact.
+
+The random fact is scraped from [unkno](http://unkno.com) using BeautifulSoup and submitted through the [Pig Latinizer](https://hidden-journey-62459.herokuapp.com/) form using the requests library.
--- main.py
+++ main.py
@@ -16,10 +16,18 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_url(fact):
+    request_url_form = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    form_key = 'input_text'
+    response = requests.post(request_url_form, data={form_key:fact}, allow_redirects=False)
+    location_url = response.headers['Location']
+    return location_url
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    location_url = get_url(fact)
+    return location_url
 
 
 if __name__ == "__main__":
